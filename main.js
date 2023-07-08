648304d10012ff1cbba49fe6acb3456263e2f58f
Nabila Ayaba <nabila.ayaba@gmail.com>
2023-07-08 09:11:14 -0400

    Create initial letter bank

--- main.js
+++ main.js
@@ -2,3 +2,39 @@ const message = document.getElementById('message')
 const image = document.getElementById('image')
 const board = document.getElementById('board')
 const letterBank = document.getElementById('letter-bank')
+
+const lettersArr = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+]
+
+lettersArr.forEach(letter => {
+  let letterDiv = document.createElement('div')
+  letterDiv.id = letter
+  letterDiv.innerHTML = letter
+  letterBank.appendChild(letterDiv)
+})
